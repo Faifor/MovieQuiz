b0baf02c68d85bbe635ef54bc08f8513c90569ba
Danila <danila.fafr@gmail.com>
2025-01-12 18:09:39 +0300

    добавил тесты

---
+++ MovieQuizTests/ArrayTests.swift
@@ -0,0 +1,33 @@
+//
+//  MovieQuizTests_2.swift
+//  MovieQuizTests 2
+//
+//  Created by Данила Спиридонов on 09.01.2025.
+//
+import XCTest
+@testable import MovieQuiz
+
+class ArrayTests: XCTestCase {
+    func testGetValueInRange() throws {
+        // Given
+        let array = [1, 1, 2, 3, 5]
+        
+        // When
+        let value = array[safe: 2]
+        
+        // Then
+        XCTAssertNotNil(value)
+        XCTAssertEqual(value, 2)
+    }
+    
+    func testGetValueOutOfRange() throws {
+        // Given
+        let array = [1, 1, 2, 3, 5]
+        
+        // When
+        let value = array[safe: 20]
+        
+        // Then
+        XCTAssertNil(value)
+    }
+}
